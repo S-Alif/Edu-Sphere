05ad34cdbfd5a9df413b319a3a8a35323bae6eda
Alif <79266506+S-Alif@users.noreply.github.com>
2024-01-26 21:47:23 +0600

    [DONE] student password encryption

--- main/backend/src/services/studentService.js
+++ main/backend/src/services/studentService.js
@@ -1,12 +1,17 @@
 const database = require('../../database')
-const { v4 } = require('uuid')
+const { v4 } = require('uuid');
+const { encryptPass } = require('../helpers/passEncryptor');
 
 // create
 exports.create = async (req) => {
   try {
     let uniqueId = v4();
     let string = "INSERT INTO `eduspshere`.`students` (`id`, `firstName`, `lastName`, `email`, `pass`, `phone`, `profileImg`) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    let data = [uniqueId, req.body.firstName, req.body.lastName, req.body.email, req.body.pass, req.body.phone, req.body.profileImg];
+
+    // encrypt password
+    let enPass = encryptPass(req.body.pass)
+
+    let data = [uniqueId, req.body.firstName, req.body.lastName, req.body.email, enPass, req.body.phone, req.body.profileImg];
 
     let result = await database.execute(string, data)
     return { status: 1, code: 200, data: "account created" }
