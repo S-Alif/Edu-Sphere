a5ad21b004fad39d6ecbae266d0991509cf27c7f
Alif <79266506+S-Alif@users.noreply.github.com>
2024-01-26 18:58:18 +0600

    [DONE] student service - function skeletons

--- main/backend/src/services/studentService.js
+++ main/backend/src/services/studentService.js
@@ -8,10 +8,37 @@ exports.create = async (req) => {
     let string = "INSERT INTO `eduspshere`.`students` (`id`, `firstName`, `lastName`, `email`, `pass`, `phone`, `profileImg`) VALUES (?, ?, ?, ?, ?, ?, ?)";
     let data = [uniqueId, req.body.firstName, req.body.lastName, req.body.email, req.body.pass, req.body.phone, req.body.profileImg];
 
-    let result = await database.execute(string, data)    
+    let result = await database.execute(string, data)
     return { status: 1, code: 200, data: "account created" }
 
   } catch (error) {
     return { status: 0, code: 200, data: "could not create account", errorCode: error.code };
+  }
+}
+
+// update
+exports.update = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// delete
+exports.delete = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// get data
+exports.getData = async (req) => {
+  try {
+
+  } catch (error) {
+
   }
 }
