75ca36d8bf7c568c90ab7caffaa2a84916b2521b
Alif <79266506+S-Alif@users.noreply.github.com>
2024-01-26 19:05:14 +0600

    [DONE] instructor service function skeleton

---
+++ main/backend/src/services/instructorService.js
@@ -0,0 +1,42 @@
+const database = require('../../database')
+const { v4 } = require('uuid')
+
+
+// create
+exports.create = async (req) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+
+// update
+exports.update = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+
+// delete
+exports.delete = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+
+// get account
+exports.getData = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
