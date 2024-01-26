31480ed7bd5fbf3489b1f86a4ee03c12175d0dee
Alif <79266506+S-Alif@users.noreply.github.com>
2024-01-26 19:07:02 +0600

    [DONE] instructor controller function skeleton

---
+++ main/backend/src/controller/instructorController.js
@@ -0,0 +1,21 @@
+const instructor = require('../services/instructorService')
+
+// registration
+exports.registerInstructor = async (req, res) => {
+  
+}
+
+// updating account
+exports.updateAccount = async (req, res) => {
+
+}
+
+// deleting account
+exports.deleteAccount = async (req, res) => {
+
+}
+
+// updating account
+exports.getAccount = async (req, res) => {
+
+}
