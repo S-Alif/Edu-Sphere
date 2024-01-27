1da3b61e76fdf95d68b6673511ff6124fc21b487
Alif <79266506+S-Alif@users.noreply.github.com>
2024-01-27 21:47:18 +0600

    [ADD] auth verification middleware

---
+++ main/backend/src/middleware/authVerification.js
@@ -0,0 +1,26 @@
+const { verifyToken } = require("../helpers/tokenHelper")
+
+
+module.exports = async (req, res, next) => {
+  let token = req.headers['token']
+  if (!token) {
+    token = req.cookies['token']
+  }
+
+  if (token) {
+    let decoded = await verifyToken(token)
+    if (!decoded) {
+      res.status(401).json({status: 0, code: 401, data: "unauthorised"})
+      return
+    }
+    else {
+      req.headers.email = decoded['email']
+      req.headers.id = decoded['id']
+      req.headers.isTeacher = decoded['isTeacher']
+      next()
+    }
+  }
+  else {
+    res.status(401).json({ status: 0, code: 401, data: "token not found" })
+  }
+}
