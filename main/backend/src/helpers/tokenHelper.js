f42548d05311546c6be605d3ee7367b00637e425
Alif <sagor01md@gmail.com>
2024-01-28 19:44:15 +0600

    [FIX] jsonwebtoken not getting secretkey from .env

--- main/backend/src/helpers/tokenHelper.js
+++ main/backend/src/helpers/tokenHelper.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 
 // function to create token
 exports.createToken = (user) => {
-  const token = jwt.sign(user, process.env.key, { expiresIn: '1d' })
+  const token = jwt.sign(user, process.env.secretKey, { expiresIn: '1d' })
   return token
 }
 
 //fucntioon to verify token
 exports.verifyToken = async (token) => {
-  const decoded = jwt.verify(token, process.env.key);
+  const decoded = jwt.verify(token, process.env.secretKey);
   return decoded;
 }
