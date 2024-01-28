a5c71b05c5d623744a6a5d426e97828d18c4c91a
Alif <sagor01md@gmail.com>
2024-01-28 22:33:09 +0600

    [DONE] adding security middleware

--- main/backend/src/routes/publicRoutes/studentRoutes.js
+++ main/backend/src/routes/publicRoutes/studentRoutes.js
@@ -4,9 +4,12 @@ const router = express.Router()
 // contoller
 const student = require('../../controller/studentController')
 
+// auth verification
+const authVerification = require('../../middleware/authVerification')
+
 // student routes
 router.post('/create', student.registerStudent)
 router.post('/login', student.loginAccount)
-router.post('/update')
+router.post('/update', authVerification, student.updateAccount)
 
 module.exports = router
