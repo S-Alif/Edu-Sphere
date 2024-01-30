01c8bd845a4d161e5a850e49a0654135101c131b
Alif <sagor01md@gmail.com>
2024-01-30 22:32:01 +0600

    [ADD] new instructor routes

--- main/backend/src/routes/publicRoutes/teacherRoutes.js
+++ main/backend/src/routes/publicRoutes/teacherRoutes.js
@@ -4,9 +4,15 @@ const router = express.Router()
 // controller
 const instructor = require("../../controller/instructorController")
 
+// middleware
+const authVerification = require('../../middleware/authVerification')
+
 // student routes
 router.post('/create', instructor.registerInstructor)
 router.post('/login', instructor.loginAccount)
-router.post('/update')
+router.post('/update', authVerification, instructor.updateAccount)
+router.post('/delete', authVerification, instructor.deleteAccount)
+router.get('/user', authVerification, instructor.getAccount)
+router.get('/user/:id')
 
 module.exports = router
