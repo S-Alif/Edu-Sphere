5548af26ccadc186e54d7680d731ce3e164558c1
koushik9010 <anikdey.6365@gmail.com>
2024-01-30 22:36:55 +0600

    [DONE] instructor controller 80%

--- main/backend/src/controller/instructorController.js
+++ main/backend/src/controller/instructorController.js
@@ -2,7 +2,8 @@ const instructor = require('../services/instructorService')
 
 // registration
 exports.registerInstructor = async (req, res) => {
-  
+    const result = await instructor.create(req);
+    res.status(200).json(result);
 }
 
 // login account
@@ -12,15 +13,18 @@ exports.loginAccount = async (req, res) => {
 
 // updating account
 exports.updateAccount = async (req, res) => {
-
+    const result = await instructor.update(req);
+    res.status(200).json(result);
 }
 
 // deleting account
 exports.deleteAccount = async (req, res) => {
-
+    const result = await instructor.delete(req);
+    res.status(200).json(result);
 }
 
 // updating account
 exports.getAccount = async (req, res) => {
-
+    const result = await instructor.getData(req);
+    res.status(200).json(result);
 }
