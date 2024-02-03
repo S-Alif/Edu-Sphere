cc518678302ae0b1c88de4b4fd7a53ff45da945f
Ancishan <shantodev027@gmail.com>
2024-02-03 22:35:36 +0600

    [DONE] review controller

--- main/backend/src/controller/reviewController.js
+++ main/backend/src/controller/reviewController.js
@@ -6,17 +6,20 @@ const review = require('../services/reviewService')
 
 // review create
 exports.instructorReviewCreate = async (req, res) => {
-
+    const result = await course.instructorReviewCreate(req);
+    res.status(200).json(result);
 }
 
 // review delete
 exports.instructorReviewDelete = async (req, res) => {
-
+    const result = await course.instructorReviewDelete(req);
+    res.status(200).json(result);
 }
 
 // review create
 exports.instructorReview = async (req, res) => {
-
+    const result = await course.instructorReview(req);
+    res.status(200).json(result);
 }
 
 
@@ -26,15 +29,18 @@ exports.instructorReview = async (req, res) => {
 
 // review create
 exports.courseReviewCreate = async (req, res) => {
-
+    const result = await course.courseReviewCreate(req);
+    res.status(200).json(result);
 }
 
 // review delete
 exports.courseReviewDelete = async (req, res) => {
-
+    const result = await course.courseReviewDelete(req);
+    res.status(200).json(result);
 }
 
 // review create
 exports.courseReview = async (req, res) => {
-
+    const result = await course.courseReview(req);
+    res.status(200).json(result);
 }
