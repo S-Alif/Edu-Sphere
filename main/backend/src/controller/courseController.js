efae060f3b75c747b443bd5ec06e22ff6b90f1f4
Ancishan <shantodev027@gmail.com>
2024-02-03 22:26:42 +0600

    [DONE] course controller

--- main/backend/src/controller/courseController.js
+++ main/backend/src/controller/courseController.js
@@ -2,25 +2,30 @@ const course = require('../services/courseService')
 
 // create course
 exports.createCourse = async (req, res) => {
-
+    const result = await course.create(req);
+    res.status(200).json(result);
 }
 
 // update course
 exports.updateCourse = async (req, res) => {
-
+    const result = await course.update(req);
+    res.status(200).json(result);
 }
 
 // delete course
 exports.deleteCourse = async (req, res) => {
-
+    const result = await course.delete(req);
+    res.status(200).json(result);
 }
 
 // get course (public)
 exports.getAllCourse = async (req, res) => {
-
+    const result = await course.getCoursePublic(req);
+    res.status(200).json(result);
 }
 
 // get course by instructor
 exports.getCourseByInstructor = async (req, res) => {
-
+    const result = await course.getCourseInstructor(req);
+    res.status(200).json(result);
 }
