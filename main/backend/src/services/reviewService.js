c410e5fef37ba8a229691001cdcb03e7850e08f1
arafatshahariyar <arafatshahariyar1444@gmail.com>
2024-02-03 22:08:38 +0600

    [DONE] review service  skeleton

--- main/backend/src/services/reviewService.js
+++ main/backend/src/services/reviewService.js
@@ -1,2 +1,67 @@
 const database = require('../../database')
 const { v4 } = require('uuid')
+
+/*-------------------------------
+    Instructor review
+---------------------------------- */
+
+// create
+exports.postInstructorReview = async (req) => {
+    try {
+      let uid = v4()
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // delete
+  exports.deleteInstructorReview = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // get reviews
+  exports.getInstructorReview = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  
+  /*-------------------------------
+      course review
+  ---------------------------------- */
+  
+  // create
+  exports.postCourseReview = async (req) => {
+    try {
+      let uid = v4()
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // delete
+  exports.deleteCourseReview = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // get reviews
+  exports.getCourseReview = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
