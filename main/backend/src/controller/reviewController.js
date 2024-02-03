c7f025684bee4b04c829bd1bf9680400ec1a92c9
arafatshahariyar <arafatshahariyar1444@gmail.com>
2024-02-03 21:49:04 +0600

    [DONE] revier controller skeleton

--- main/backend/src/controller/reviewController.js
+++ main/backend/src/controller/reviewController.js
@@ -1,1 +1,40 @@
 const review = require('../services/reviewService')
+
+/*-------------------------------
+    Instructor review
+---------------------------------- */
+
+// review create
+exports.instructorReviewCreate = async (req, res) => {
+
+}
+
+// review delete
+exports.instructorReviewDelete = async (req, res) => {
+
+}
+
+// review create
+exports.instructorReview = async (req, res) => {
+
+}
+
+
+/*-------------------------------
+    course review
+---------------------------------- */
+
+// review create
+exports.courseReviewCreate = async (req, res) => {
+
+}
+
+// review delete
+exports.courseReviewDelete = async (req, res) => {
+
+}
+
+// review create
+exports.courseReview = async (req, res) => {
+
+}
