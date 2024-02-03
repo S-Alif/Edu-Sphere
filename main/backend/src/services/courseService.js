277bbe2279e263ae2bbd1af3f1844e7379094e86
koushik9010 <anikdey.6365@gmail.com>
2024-02-03 22:16:40 +0600

    [DONE] course service skeleton

--- main/backend/src/services/courseService.js
+++ main/backend/src/services/courseService.js
@@ -1,2 +1,48 @@
 const database = require('../../database')
 const { v4 } = require('uuid')
+
+// create
+exports.create = async (req) => {
+    try {
+      let uid = v4()
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // update
+  exports.update = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // delete
+  exports.delete = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // get course (public)
+  exports.getCoursePublic = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
+  
+  // get course by instructor
+  exports.getCourseInstructor = async (req) => {
+    try {
+  
+    } catch (error) {
+  
+    }
+  }
