79687874e61f29ff2b2127c5bc95318c7445038e
Alif <sagor01md@gmail.com>
2024-02-03 20:26:47 +0600

    [ADD] course routes for instructors

--- main/backend/src/routes/publicRoutes/teacherRoutes.js
+++ main/backend/src/routes/publicRoutes/teacherRoutes.js
@@ -7,12 +7,18 @@ const instructor = require("../../controller/instructorController")
 // middleware
 const authVerification = require('../../middleware/authVerification')
 
-// student routes
+// instructor routes
 router.post('/create', instructor.registerInstructor)
 router.post('/login', instructor.loginAccount)
 router.post('/update', authVerification, instructor.updateAccount)
 router.post('/delete', authVerification, instructor.deleteAccount)
 router.get('/user', authVerification, instructor.getAccount)
 router.get('/user/:id')
+
+
+// course routes
+router.post('/create-course', authVerification)
+router.post('/update-course', authVerification)
+router.post('/delete-course', authVerification)
 
 module.exports = router
