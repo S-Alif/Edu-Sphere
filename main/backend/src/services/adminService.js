5913fcd41d2bddd3d05ae274c6d3b3afe7f97484
Alif <sagor01md@gmail.com>
2024-02-04 21:28:57 +0600

    [DONE] admin service skeleton

---
+++ main/backend/src/services/adminService.js
@@ -0,0 +1,49 @@
+const database = require('../../database')
+const { v4 } = require('uuid')
+
+
+// register
+exports.registerAdmin = async (req) => {
+  try {
+    let uid = v4()
+
+  } catch (error) {
+
+  }
+}
+
+// login
+exports.loginAdmin = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// update
+exports.updateAdmin = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// delete
+exports.deleteAdmin = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+// get profile
+exports.getProfile = async (req) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
