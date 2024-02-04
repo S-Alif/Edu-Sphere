d60f14990a1f006bdc49811652617511d2c33e83
Alif <sagor01md@gmail.com>
2024-02-04 21:23:20 +0600

    [DONE] admin controler skeleton

--- main/backend/src/controller/adminController.js
+++ main/backend/src/controller/adminController.js
@@ -1,1 +1,27 @@
 const admin = require('../services/adminService')
+
+
+// register
+exports.register = async (req, res) => {
+
+}
+
+// login 
+exports.login = async (req, res) => {
+
+}
+
+// update
+exports.update = async (req, res) => {
+
+}
+
+// delete
+exports.delete = async (req, res) => {
+
+}
+
+// get admin profile
+exports.profile = async (req, res) => {
+  
+}
