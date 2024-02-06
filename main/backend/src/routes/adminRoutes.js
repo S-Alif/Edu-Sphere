caee509e8f7a39f671a889796dae53c188a7582c
Alif <sagor01md@gmail.com>
2024-02-06 21:34:56 +0600

    [ADD] basic admin routes

--- main/backend/src/routes/adminRoutes.js
+++ main/backend/src/routes/adminRoutes.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const router = express.Router()
+
+// controllers
+const admin = require('../controller/adminController')
+
+// middleware
+const adminAuth = require('../middleware/adminAuthVerification')
+
+
+// basic routes
+router.post('/login', admin.login)
+router.post('/register', admin.register)
+router.post('/update', adminAuth, admin.update)
+router.delete('/delete', adminAuth, admin.delete)
