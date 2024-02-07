238e7d32e67e7d2949bffbe77bc688517d9bf4e9
Ancishan <shantodev027@gmail.com>
2024-02-07 22:16:12 +0600

    [DONE] subject services skeleton

---
+++ main/backend/src/services/subjectService.js
@@ -0,0 +1,42 @@
+const database = require('../../database')
+const { v4 } = require('uuid')
+
+//  create
+exports.create = async (req) => {
+    try {
+        let uid = v4()
+
+    } catch (error) {
+        
+    }
+}
+
+//  update
+exports.update = async (req) => {
+    try {
+        
+
+    } catch (error) {
+        
+    }
+}
+
+//  delete
+exports.delete = async (req) => {
+    try {
+        
+
+    } catch (error) {
+        
+    }
+}
+
+//  get Subjects
+exports.getSubjects = async (req) => {
+    try {
+        
+
+    } catch (error) {
+        
+    }
+}
