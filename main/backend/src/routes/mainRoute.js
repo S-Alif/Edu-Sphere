1ae6867430784d9a68fe4f8ea5cf74a56885cbd0
Ancishan <shantodev027@gmail.com>
2024-02-16 21:21:38 +0600

    [ADD] basic route to main routes

--- main/backend/src/routes/mainRoute.js
+++ main/backend/src/routes/mainRoute.js
@@ -4,8 +4,10 @@ const router = express.Router()
 // require routes
 const studentRoutes = require('./publicRoutes/studentRoutes')
 const instructorRoutes = require('./publicRoutes/teacherRoutes')
+const basicRoutes = require('./publicRoutes/basicRoutes')
 
 router.use('/student', studentRoutes)
 router.use('/teacher', instructorRoutes)
+router.use('/basic', basicRoutes)
 
 module.exports = router
