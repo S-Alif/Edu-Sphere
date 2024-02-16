7237b9071e9de7062022aef6564e87143f6a1c02
Ancishan <shantodev027@gmail.com>
2024-02-16 22:45:23 +0600

    [ADD/ DONE] added  Home page, add to main route

--- main/frontend/src/App.jsx
+++ main/frontend/src/App.jsx
@@ -3,12 +3,14 @@ import LoginPage from './pages/LoginPage';
 import InstuctorRegPage from './pages/InstuctorRegPage';
 import StudentRegPage from './pages/StudentRegPage';
 import MainLayout from './components/layouts/MainLayout';
+import HomePage from "./pages/HomePage";
 
 
 const App = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="student-register" element={<StudentRegPage />} />
         <Route path="instructor-register" element={<InstuctorRegPage />} />
