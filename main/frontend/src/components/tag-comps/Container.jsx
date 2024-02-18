9193c14d4924244e925c355c03f3468f0ce1e74b
Alif <sagor01md@gmail.com>
2024-02-18 21:50:46 +0600

    [UPDATE] proper reuseable container component

--- main/frontend/src/components/tag-comps/Container.jsx
+++ main/frontend/src/components/tag-comps/Container.jsx
@@ -1,7 +1,10 @@
 // container component for unified design
-const Container = ({ children }) => {
+const Container = ({ children, className }) => {
+
+  const classes = className || ""
+
   return (
-    <div className="container mx-auto px-3">
+    <div className={`container mx-auto px-3 ${classes}`}>
       {children}
     </div>
   );
