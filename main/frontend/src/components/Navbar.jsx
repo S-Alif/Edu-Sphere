e382e85d8409e69c6f2564fd6b17939140f91f9d
Alif <sagor01md@gmail.com>
2024-02-20 19:45:21 +0600

    [UPDATE] Navbar design

--- main/frontend/src/components/Navbar.jsx
+++ main/frontend/src/components/Navbar.jsx
@@ -13,31 +13,31 @@ const Navbar = () => {
     return (
         <>
             {/* top navbar */}
-            <div className={`navbar-top h-20 ${theme ? "bg-slate-900" : "bg-current"}`}>
+            <div className={`navbar-top h-16 ${theme ? "bg-slate-900" : "bg-slate-200"} sticky`}>
                 <Container className={"h-full"}>
                     <div className="flex w-full h-full justify-between items-center">
 
                         {/* logo */}
                         <div className="logo">
-                            <NavLink to={"/"}> <img src={theme ? logoDark : logoLight} alt="edusphere-logo" className='w-48' /> </NavLink>
+                            <NavLink to={"/"}> <img src={theme ? logoDark : logoLight} alt="edusphere-logo" className='w-44' /> </NavLink>
                         </div>
 
                         {/* menu */}
                         <div className="menu-bar flex h-full items-center gap-8">
 
                             {/* menu links */}
-                            <div className="links flex gap-5">
-                                <NavLink to={"/"} className={`font-bold ${theme ? "hover:text-lime-500" : "hover:text-lime-800"}`}>Home</NavLink>
-                                <NavLink to={"/about"} className={`font-bold ${theme ? "hover:text-lime-500" : "hover:text-lime-800"}`}>About</NavLink>
-                                <NavLink to={"/courses"} className={`font-bold ${theme ? "hover:text-lime-500" : "hover:text-lime-800"}`}>Courses</NavLink>
-                                <NavLink to={"/instructors"} className={`font-bold ${theme ? "hover:text-lime-500" : "hover:text-lime-800"}`}>Instructors</NavLink>
-                                <NavLink to={"/contact"} className={`font-bold ${theme ? "hover:text-lime-500" : "hover:text-lime-800"}`}>Contact</NavLink>
+                            <div className="links flex gap-6">
+                                <NavLink to={"/"} className={`font-semibold ${theme ? "hover:text-lime-500" : "hover:text-lime-600"}`}>Home</NavLink>
+                                <NavLink to={"/about"} className={`font-semibold ${theme ? "hover:text-lime-500" : "hover:text-lime-600"}`}>About</NavLink>
+                                <NavLink to={"/courses"} className={`font-semibold ${theme ? "hover:text-lime-500" : "hover:text-lime-600"}`}>Courses</NavLink>
+                                <NavLink to={"/instructors"} className={`font-semibold ${theme ? "hover:text-lime-500" : "hover:text-lime-600"}`}>Instructors</NavLink>
+                                <NavLink to={"/contact"} className={`font-semibold ${theme ? "hover:text-lime-500" : "hover:text-lime-600"}`}>Contact</NavLink>
                             </div>
 
                             <div className="profile">
                                 <div className="dropdown dropdown-end">
                                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar border-2 border-lime-500">
-                                        <div className="w-10 rounded-full">
+                                        <div className="w-9 rounded-full">
                                             <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                                         </div>
                                     </div>
