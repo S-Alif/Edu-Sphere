8f1343e1d72be9012e07a2438ce2cb16edf24d12
Alif <sagor01md@gmail.com>
2024-02-21 19:01:32 +0600

    [UPDATE] login page design

--- main/frontend/src/pages/LoginPage.jsx
+++ main/frontend/src/pages/LoginPage.jsx
@@ -1,32 +1,37 @@
 // import image
-import loginPageIllustration from '../assets/imgs/login-illustration-1.png'
+import { useState } from 'react';
+import loginPageIllustration from '../assets/imgs/login-illustration-1.jpg'
 
 const LoginPage = () => {
+
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+
     return (
         <>
             <section className="login-page">
-                <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className="flex justify-center lg:justify-normal h-full">
 
-                    <div className="image-box">
+                    <div className="image-box w-1/2 lg:block hidden h-full overflow-hidden">
                         <img src={loginPageIllustration} alt="login page illustration" />
                     </div>
 
                     {/* form content */}
-                    <div className="form-content py-8 px-4 lg:px-8">
+                    <div className="form-content py-8 lg:py-20 px-4 lg:px-20 shadow-lg lg:shadow-none my-12 lg:my-0">
 
                         <h1 className='text-3xl font-bold'>Welcome to <span className='text-emerald-500'>eduSphere</span></h1>
                         <p className='pt-1 text-slate-400'>Login to your account</p>
 
                         <form action="" className='pt-8 w-full'>
 
-                            <div className="w-full lg:w-1/2">
+                            <div className="w-full">
                                 <label htmlFor="email" className='font-semibold'>Email</label>
-                                <input type="email" className='form-control input input-bordered border-emerald-500 mt-4 mb-6 w-full' name='email' id='email' placeholder='enter your email' autoComplete='false' />
+                                <input type="email" className='form-control input input-bordered border-emerald-500 mt-4 mb-6 w-full' name='email' id='email' placeholder='enter your email' autoComplete='false' onChange={(e) => setEmail(e.target.value)} />
                             </div>
 
-                            <div className="w-full lg:w-1/2">
+                            <div className="w-full">
                                 <label htmlFor="pass" className='font-semibold'>Password</label>
-                                <input type="password" className='form-control input input-bordered border-emerald-500 w-full mt-4' name='pass' id='pass' placeholder='enter your password' />
+                                <input type="password" className='form-control input input-bordered border-emerald-500 w-full mt-4' name='pass' id='pass' placeholder='enter your password' onChange={(e) => setPass(e.target.value)} />
                             </div>
 
                             <button type='button' className='btn bg-emerald-500 text-white px-10 mt-6 hover:bg-green-700'>Login</button>
@@ -35,7 +40,7 @@ const LoginPage = () => {
 
                         {/* route to registration */}
                         <div className="pt-6">
-                            <p>
+                            <div>
                                 Don&apos;t have an account !!
                                 <details className="dropdown lg:dropdown-right">
                                     <summary className="m-1 btn bg-transparent hover:bg-emerald-500 hover:text-white border-2 border-emerald-500">Register as</summary>
@@ -44,7 +49,7 @@ const LoginPage = () => {
                                         <li><button className='hover:bg-emerald-500 hover:text-white'>Instructor</button></li>
                                     </ul>
                                 </details>
-                            </p>
+                            </div>
                         </div>
                     </div>
 
