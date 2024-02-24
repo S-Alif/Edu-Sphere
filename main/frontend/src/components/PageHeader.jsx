fbad5b40ede78e051e3c0681663a56a9d7b07a84
Alif <sagor01md@gmail.com>
2024-02-24 12:38:47 +0600

    [DONE] page header design

--- main/frontend/src/components/PageHeader.jsx
+++ main/frontend/src/components/PageHeader.jsx
@@ -1,11 +1,20 @@
 import Container from './tag-comps/Container';
 
-const PageHeader = ({pageTitle, pageText}) => {
+const PageHeader = ({ pageTitle, pageText, headerBg }) => {
+
+  const styles = {
+    height: "50vh",
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7))${headerBg && ", url(" + headerBg + ")"}`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  }
+
   return (
-    <section className="page-header">
-      <Container>
-        <div className="py-5">
-          <h1 className='text-3xl font-bold'>{pageTitle}</h1>
+    <section className="page-header" style={styles}>
+      <Container className={"h-full"}>
+        <div className="py-5 h-full text-white flex flex-col justify-center">
+          <h1 className=' text-3xl lg:text-6xl font-bold'>{pageTitle}</h1>
           <p className='pt-4'>{pageText}</p>
         </div>
       </Container>
