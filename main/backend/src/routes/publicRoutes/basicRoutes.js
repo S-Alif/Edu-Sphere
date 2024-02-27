996309091156dc2f92006bbf947b4fe9fab6041d
koushik9010 <anikdey.6365@gmail.com>
2024-02-27 21:39:17 +0600

    [ADD] course and subject routes to basic routes

--- main/backend/src/routes/publicRoutes/basicRoutes.js
+++ main/backend/src/routes/publicRoutes/basicRoutes.js
@@ -3,12 +3,23 @@ const router = express.Router()
 
 // controllers
 const instructor = require("../../controller/instructorController")
+const subject = require("../../controller/subjectController")
+const course = require("../../controller/courseController")
 
 // instructor routes
 router.get('/instructors', instructor.getAllInstructors)
 
 // course routes
-router.get('/courses')
+router.get('/courses', course.courses)
+router.get('/courses-instructor/:id', course.courseByInstructor)
+router.get('/courses-subject/:subject', course.courseBySubject)
+router.get('/courses-class/:class', course.courseByClass)
+router.get('/courses-code/:code', course.courseByCode)
+
+// subjects
+router.get('/classes', subject.classes)
+router.get('/subjects', subject.getAllSubject)
+router.get('/subjects/:class', subject.subjectByClass)
 
 
 module.exports = router
