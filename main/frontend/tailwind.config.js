eec67062706bc1c9ea72230ce123541e23993b05
koushik9010 <anikdey.6365@gmail.com>
2024-03-02 22:31:26 +0600

    [FIX] tailwind container breakpoint

--- main/frontend/tailwind.config.js
+++ main/frontend/tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': { 'min': '641px' },
+        'md': { 'min': '769px' },
+        'lg': { 'min': '1025px' },
+        'xl': { 'min': '1281px' },
+        '2xl': { 'min': '1537px' },
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
-
