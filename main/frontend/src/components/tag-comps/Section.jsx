0ce7436a5e97a86023b99a185896766fd11c77e5
Alif <sagor01md@gmail.com>
2024-03-07 18:57:09 +0600

    [UPDATE] section component id adding

--- main/frontend/src/components/tag-comps/Section.jsx
+++ main/frontend/src/components/tag-comps/Section.jsx
@@ -1,12 +1,12 @@
 import Container from "./Container";
 
 // common section for the app
-const Section = ({children, className}) => {
+const Section = ({children, className, id}) => {
 
   const classes = className || ""
 
   return (
-    <section className={`${classes} py-24 overflow-x-hidden`}>
+    <section className={`${classes} py-24 overflow-x-hidden`} id={id ? id : undefined}>
       <Container>
         {children}
       </Container>
