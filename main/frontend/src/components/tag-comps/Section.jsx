7c371ee19859cdea1e42028195030dd23bb8c247
koushik9010 <anikdey.6365@gmail.com>
2024-03-07 22:42:35 +0600

    [UPDATE] increase section padding

--- main/frontend/src/components/tag-comps/Section.jsx
+++ main/frontend/src/components/tag-comps/Section.jsx
@@ -6,7 +6,7 @@ const Section = ({children, className, id}) => {
   const classes = className || ""
 
   return (
-    <section className={`${classes} py-24 overflow-x-hidden`} id={id ? id : undefined}>
+    <section className={`${classes} py-32 overflow-x-hidden`} id={id ? id : undefined}>
       <Container>
         {children}
       </Container>
