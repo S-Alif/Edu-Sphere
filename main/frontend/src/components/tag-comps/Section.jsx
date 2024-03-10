4085ef9e8a84ef781ca51a325233ee4ce7edeb0e
Ancishan <shantodev027@gmail.com>
2024-03-10 19:43:44 +0600

    [UPDATE] seperate padding ability

--- main/frontend/src/components/tag-comps/Section.jsx
+++ main/frontend/src/components/tag-comps/Section.jsx
@@ -1,12 +1,13 @@
 import Container from "./Container";
 
 // common section for the app
-const Section = ({children, className, id}) => {
+const Section = ({ children, className, id, padding }) => {
 
   const classes = className || ""
+  const paddings = padding || "py-32"
 
   return (
-    <section className={`${classes} py-32 overflow-x-hidden`} id={id ? id : undefined}>
+    <section className={`${classes} ${paddings} overflow-x-hidden`} id={id ? id : undefined}>
       <Container>
         {children}
       </Container>
