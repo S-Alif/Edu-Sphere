72bbce7f4cabe196af84caeb52aaf6a3a4da7d2e
Ancishan <shantodev027@gmail.com>
2024-03-10 19:45:53 +0600

    [UPDATE] role  based route and highlight active route

--- main/frontend/src/components/layouts/DashboardLayout.jsx
+++ main/frontend/src/components/layouts/DashboardLayout.jsx
@@ -1,13 +1,14 @@
-import { NavLink, useLocation, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { useState } from "react";
+import userStore from "../../store/userStore";
 
 const DashboardLayout = () => {
 
-  const location = useLocation()
   const [menuToggle, setMenuToggle] = useState(false)
+  const { user } = userStore()
+
   const linkStyles = "py-4 hover:bg-emerald-200 font-semibold transition duration-500 wi-full flex justify-center items-center"
-  const matchStyles = "bg-emerald-500 text-white"
 
   return (
     <section className="dashboard-layout flex gap-9">
@@ -19,9 +20,10 @@ const DashboardLayout = () => {
 
         {/* sidebar */}
         <div className={`w-80 h-[calc(100vh-80px)] pt-9 sticky top-20 bg-gray-300 ${menuToggle ? 'lg:block' : 'hidden lg:block'}`}>
-          <NavLink to={'/instructor'} className={`${linkStyles} ${location.pathname == "/instructor" && matchStyles}`}>Dashboard</NavLink>
-          <NavLink to={'/instructor/courses'} className={`${linkStyles} ${location.pathname == "/instructor/courses" && matchStyles}`}>My courses</NavLink>
-          <NavLink to={'/instructor/account'} className={`${linkStyles} ${location.pathname == "/instructor/account" && matchStyles}`}>Account</NavLink>
+          <NavLink to={'/instructor'} end className={linkStyles}>Dashboard</NavLink>
+          <NavLink to={'/instructor/courses'} className={linkStyles}>My courses</NavLink>
+          {user?.role == 1 && (<NavLink to={'/instructor/create-courses'} className={linkStyles}>Create courses</NavLink>)}
+          <NavLink to={'/instructor/account'} className={linkStyles}>Account</NavLink>
         </div>
 
 
