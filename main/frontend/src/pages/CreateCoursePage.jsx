d41c67479adeb59139c663b6cd8c78129367fbff
Ancishan <shantodev027@gmail.com>
2024-03-10 19:50:07 +0600

    [DONE] create course page initial design

---
+++ main/frontend/src/pages/CreateCoursePage.jsx
@@ -0,0 +1,19 @@
+import Section from "../components/tag-comps/Section";
+
+
+const CreateCoursePage = () => {
+  return (
+    <>
+      <Section className={"create-course-section"} padding={"py-10"}>
+        {/* title */}
+        <div className="title pb-4 mb-7 border-b-2 border-b-emerald-300">
+          <h2 className="font-bold text-3xl">Create a course</h2>
+        </div>
+
+        
+      </Section>
+    </>
+  );
+};
+
+export default CreateCoursePage;
--- main/frontend/src/pages/InstructorDashboard.jsx
+++ main/frontend/src/pages/InstructorDashboard.jsx
@@ -4,7 +4,7 @@ import Section from './../components/tag-comps/Section';
 
 const InstructorDashboard = () => {
   return (
-    <Section className="dashboard py-10">
+    <Section className="dashboard" padding={"py-10"}>
       {/* user profile component */}
       <UserProfile />
     </Section>
