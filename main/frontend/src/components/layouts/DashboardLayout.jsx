c089ce4960a6e017f73bfa950a0e1d2b377ba562
arafatshahariyar <arafatshahariyar1444@gmail.com>
2024-03-14 22:12:36 +0600

    [Add] Batch configer add to dashboard sideber

--- main/frontend/src/components/layouts/DashboardLayout.jsx
+++ main/frontend/src/components/layouts/DashboardLayout.jsx
@@ -11,18 +11,19 @@ const DashboardLayout = () => {
   const linkStyles = "py-4 hover:bg-emerald-200 font-semibold transition duration-500 wi-full flex justify-center items-center"
 
   return (
-    <section className="dashboard-layout flex gap-9">
+    <section className="dashboard-layout block lg:flex gap-9">
       <div className={`dashboard-sidebar`}>
 
-        <div className="lg:hidden block w-full pb-6">
-          <button className="btn btn-ghost bg-gray-300 text-4xl" onClick={() => setMenuToggle(prev => !prev)}><HiMenuAlt2 /></button>
+        <div className="lg:hidden block w-full pb-6 z-20 fixed">
+          <button className="btn bg-emerald-400 hover:bg-emerald-500 text-white text-3xl rounded-none" onClick={() => setMenuToggle(prev => !prev)}><HiMenuAlt2 /></button>
         </div>
 
         {/* sidebar */}
-        <div className={`w-80 h-[calc(100vh-80px)] pt-9 sticky top-20 bg-gray-300 ${menuToggle ? 'lg:block' : 'hidden lg:block'}`}>
+        <div className={`w-80 h-[calc(100vh-80px)] pt-9 fixed z-10 lg:sticky top-20 bg-gray-300 ${menuToggle ? 'lg:block' : 'hidden lg:block'}`}>
           <NavLink to={'/instructor'} end className={linkStyles}>Dashboard</NavLink>
           <NavLink to={'/instructor/courses'} className={linkStyles}>My courses</NavLink>
           {user?.role == 1 && (<NavLink to={'/instructor/create-courses'} className={linkStyles}>Create courses</NavLink>)}
+          {user?.role == 1 && (<NavLink to={'/instructor/batch-configure'} className={linkStyles}>Batch configure</NavLink>)}
           <NavLink to={'/instructor/account'} className={linkStyles}>Account</NavLink>
         </div>
 
