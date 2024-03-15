ba350de5484e16ffc6932118bd9810058e165e34
arafatshahariyar <arafatshahariyar1444@gmail.com>
2024-03-15 14:50:27 +0600

    [Complete] About page

--- main/frontend/src/pages/AboutPage.jsx
+++ main/frontend/src/pages/AboutPage.jsx
@@ -1,10 +1,115 @@
+import PageHeader from "../components/PageHeader";
+import Section from "../components/tag-comps/Section";
 
 
 const AboutPage = () => {
   return (
-    <div>
-      about page
-    </div>
+    <>
+      <PageHeader pageTitle={"About Us"} pageText={"Everything about Edusphere, our mission, vision and goal"} headerBg={"https://i.postimg.cc/Nfkdr3gg/Screenshot-2024-03-15-112045.png"} />
+
+      {/* start */}
+      <Section>
+        <h2 className="text-3xl font-bold pb-5">Welcome to <span className="text-emerald-400">Edusphere</span></h2>
+        <p className="font-normal pb-3">Edusphere, a cutting-edge platform where learning&apos;s limits are continuously pushed and reinvented. Here, learning is an engaging experience that breaks down conventional barriers rather than merely a set of lectures or classes. At Edusphere, we think that innovation, curiosity, and discovery have the ability to advance learning. Our goal is to enable students of all ages and backgrounds to reach their greatest potential and prosper in a world that is changing quickly. Our mission is to transform knowledge acquisition and application via the use of state-of-the-art technology, interactive simulations, and personalised learning routes.</p>
+
+        <p className="font-normal pb-3">However, Edusphere is more than simply a platform; it&apos;s a thriving community of educators, innovators, and lifelong learners brought together by a common love of learning. Here, variety is welcomed, innovation is respected, and teamwork is encouraged.</p>
+
+        {/* table of contents */}
+        <h3 className="text-2xl font-bold underline underline-offset-8 pt-6">Table of contents</h3>
+
+        <ul className="list-disc pl-4 pt-5">
+          <li className="text-xl"><a href="#introduction" className="link text-blue-400 font-medium underline-offset-2">Introduction</a></li>
+          <li className="text-xl pt-2"><a href="#mission" className="link text-blue-400 font-medium underline-offset-2">Mission</a></li>
+          <li className="text-xl pt-2"><a href="#vision" className="link text-blue-400 font-medium underline-offset-2">Vision</a></li>
+          <li className="text-xl pt-2"><a href="#goal" className="link text-blue-400 font-medium underline-offset-2">End goal</a></li>
+        </ul>
+
+      </Section>
+
+      {/* introduction */}
+      <Section className={"bg-gray-100 dark:bg-[#1c262c]"} id={"introduction"}>
+        <h2 className="text-3xl font-bold pb-5">Introduction to <span className="text-emerald-400">Edusphere</span></h2>
+
+        <p className="font-normal pb-3">Greetings from Edusphere, a vibrant haven of innovative learning within the large field of education. Here at Edusphere, we think that education ought to be an investigation, a voyage of development, and discovery. Our goal is to completely transform education by breaking down conventional barriers and realising each learner&apos;s potential. It seems straightforward, but it has deep implications.</p>
+
+        <p className="font-normal pb-3">Edusphere enables people of all ages and backgrounds to prosper in a world that is always changing by utilising cutting-edge technology, immersive experiences, and personalised learning pathways. But we&apos;re more than simply a platform; we&apos;re a community, a thriving ecosystem where variety is valued, cooperation thrives, and innovation blossoms.</p>
+
+        <div className="w-full py-8">
+          <div className="max-w-3xl aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1588702547919-26089e690ecc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+        </div>
+
+        <p className="font-normal pt-3">Come along with us as we set off on this life-changing search for understanding. Edusphere extends an invitation to you to go out on a path of boundless opportunities, regardless of your status as a student, teacher, or lifelong learner. Greetings from Edusphere, the endless source of learning.</p>
+
+      </Section>
+
+      {/* mission */}
+      <Section id={"mission"}>
+        <h2 className="text-3xl font-bold pb-5">Our mission</h2>
+
+        <p className="font-normal pb-3">At Edusphere, our mission is to revolutionize the landscape of education by providing innovative, accessible, and personalized learning experiences that empower individuals to reach their full potential. We strive to cultivate a dynamic learning community where curiosity is nurtured, collaboration is encouraged, and diversity is celebrated. Through cutting-edge technology and creative pedagogy, we aim to inspire lifelong learners who are equipped with the knowledge, skills, and mindset necessary to thrive in an ever-evolving world. Our commitment is to make learning engaging, interactive, and transformative, ensuring that every individual has the opportunity to embark on a fulfilling educational journey with Edusphere.</p>
+
+        <p className="font-normal pb-3">The idea that everyone, regardless of background or circumstances, deserves access to high-quality education is fundamental to our goal. By offering an inclusive platform that accommodates a range of learning requirements, preferences, and styles, we hope to democratise education. By utilising adaptive technology and personalised learning pathways, we enable people to take charge of their education and realise their full potential.</p>
+
+        <div className="flex flex-col lg:flex-row w-full justify-center gap-6 py-8">
+          <div className="max-w-3xl lg:w-1/2 aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://plus.unsplash.com/premium_photo-1681248156475-be7454b5d54b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+          <div className="max-w-3xl lg:w-1/2 aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://images.pexels.com/photos/4145032/pexels-photo-4145032.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+        </div>
+
+        <p className="font-normal pt-3">In addition, we are committed to creating a dynamic learning environment where creativity and cooperation are encouraged. Through enabling significant connections between students, teachers, and experts worldwide, we establish an ecosystem that fosters creativity, critical thinking, and a lifetime love of learning.</p>
+
+        <p className="font-normal pt-3">Our ultimate goal is to uplift and provide people with the information, abilities, and mindset required to prosper in the twenty-first century. We think that education should enable people to create a better tomorrow in addition to preparing them for the problems of the present. We are driving change in the direction of an educational environment that is more approachable, interesting, and influential with our unwavering dedication to innovation, inclusion, and excellence. Come along on this revolutionary adventure with us as we reimagine learning with Edusphere.</p>
+
+      </Section>
+
+      {/* vision */}
+      <Section className={"bg-gray-100 dark:bg-[#1c262c]"} id={"vision"}>
+        <h2 className="text-3xl font-bold pb-5">Our vision</h2>
+
+        <p className="font-normal pb-3">The vision we have at Edusphere is to lead the way in a paradigm change in education by transforming learning into an exciting adventure of empowerment, transformation, and discovery. We see a time when education transcends traditional bounds and fully utilises both human potential and the limitless potential of technology.</p>
+
+        <p className="font-normal pb-3">Our objective is to establish a countrywide learning community where people from all backgrounds may join together to explore, contribute, and prosper. This community will be able to transcend both geographical and economical obstacles. We want to be the driving force behind a revolution in education, one in which every student has the freedom to follow their dreams and passions and where creativity and innovation are encouraged.</p>
+
+        <div className="w-full py-8">
+          <div className="max-w-3xl aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1536337005238-94b997371b40?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+        </div>
+
+        <p className="font-normal pt-3">Furthermore, we see a day when education goes beyond universal strategies and becomes incredibly individualised, flexible, and inclusive. Understanding that every learner has different needs and preferences, we are dedicated to utilising state-of-the-art technology and data-driven insights to customise learning experiences. Through the smooth integration of adaptive tests, personalised learning pathways, and interactive resources, we aim to provide an educational ecosystem that not only caters to the unique needs of each student but also fosters a deep feeling of involvement and community.</p>
+
+        <p className="font-normal pt-3">Our long-term objective is to create a society in which education is a fundamental human right and a potent agent of positive change, rather than merely a means to an end. One learner, one community, and one breakthrough at a time, we are committed to bringing this vision to life via our uncompromising commitment to innovation, accessibility, and excellence. Come along with us as we set off on this life-changing journey with Edusphere towards a day where learning has no boundaries.</p>
+      </Section>
+
+      {/* goal */}
+      <Section id={"goal"}>
+        <h2 className="text-3xl font-bold pb-5"><span className="text-emerald-400">Edusphere</span> end goal</h2>
+
+        <p className="font-normal pb-3">At Edusphere, our ultimate goal is to build an international community of capable learners with the information, abilities, and attitude needed to prosper in a world that is always changing. In our ideal future, education will not be limited by conventional lines, enabling everyone to reach their greatest potential and contribute significantly to society. Our ultimate objective is to promote a culture of lifelong learning where people value inquiry, support innovation, and welcome ongoing development.</p>
+
+        <p className="font-normal pb-3">In addition, we work to close the knowledge gap between education and the workforce by giving students the hands-on training and real-world experience they need to be successful in their chosen industries. Our goal is to enable learners to pursue rewarding careers and positively impact their communities by facilitating smooth transitions from college to the workforce through the creation of partnerships with industry leaders and the utilisation of experiential learning opportunities.</p>
+
+        <p className="font-normal pb-3">Our long-term goal is to enable people from all circumstances to realise their full potential and to democratise access to high-quality education in order to spark meaningful social change. We hope to establish a future where learning is a lifetime adventure of discovery, progress, and fulfilment rather than merely a means to an end through our constant commitment to innovation, inclusion, and excellence.</p>
+
+        <div className="flex flex-col lg:flex-row w-full justify-center gap-6 py-8">
+          <div className="max-w-3xl lg:w-1/3 aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://images.pexels.com/photos/301926/pexels-photo-301926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+          <div className="max-w-3xl lg:w-1/3 aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://images.pexels.com/photos/4145153/pexels-photo-4145153.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+          <div className="max-w-3xl lg:w-1/3 aspect-video mx-auto shadow-lg rounded-md overflow-hidden">
+            <img src="https://images.pexels.com/photos/8003527/pexels-photo-8003527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="w-full h-full object-cover object-center" />
+          </div>
+        </div>
+
+      </Section>
+    </>
   );
 };
 
