12fbc8c3d891e22e5be48767f33392f8c755c77f
koushik9010 <anikdey.6365@gmail.com>
2024-03-18 16:53:47 +0600

    [ADD] new stores for instructor

--- main/frontend/src/store/basicStore.js
+++ main/frontend/src/store/basicStore.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { create } from 'zustand'
-import { basicEndpoint } from '../helpers/apiEndpoints'
+import { basicEndpoint, instructorEndpoint } from '../helpers/apiEndpoints'
 import { errorAlert, successAlert } from './../helpers/alertMsg';
 
 
@@ -35,6 +35,29 @@ const basicStore = create((set) => ({
         return 0
       }
       set({ subjects: result.data['data'] })
+
+    } catch (error) {
+      errorAlert("Something went wrong")
+      return 0
+    }
+  },
+
+  // subjects by instructor
+  subByInstructor: async (id) => {
+    try {
+      if(id){
+        var result = await axios.get(basicEndpoint + "/subjects/"+id)
+      }
+      else{
+        result = await axios.get(instructorEndpoint + "/subjects", {withCredentials: true})
+      }
+
+      if(result.data?.status == 0){
+        errorAlert(result.data['data'])
+        return 0
+      }
+
+      return result.data?.data
 
     } catch (error) {
       errorAlert("Something went wrong")
--- main/frontend/src/store/instructorStore.js
+++ main/frontend/src/store/instructorStore.js
@@ -11,15 +11,15 @@ const instructorStore = create((set) => ({
       infoAlert("creating course ... please wait")
       let result = await axios.post(instructorEndpoint + "/create-course", data, { withCredentials: true })
 
-      if(result.data?.status == 100){
+      if (result.data?.status == 100) {
         errorAlert(result.data?.data)
         setTimeout(() => {
           window.location.replace("/login")
         }, 3000)
       }
-      
-      if(result.data?.status == 0){
-        if(result.data?.errorCode?.code) infoAlert(result.data.errorCode.code)
+
+      if (result.data?.status == 0) {
+        if (result.data?.errorCode?.code) infoAlert(result.data.errorCode.code)
         return errorAlert(result.data?.data)
       }
 
@@ -34,7 +34,7 @@ const instructorStore = create((set) => ({
   // course names for batches
   fetchCourseNames: async () => {
     try {
-      let result = await axios.get(instructorEndpoint + "/course-names", {withCredentials: true})
+      let result = await axios.get(instructorEndpoint + "/course-names", { withCredentials: true })
 
       if (result.data?.status == 100) {
         errorAlert(result.data?.data)
@@ -51,6 +51,110 @@ const instructorStore = create((set) => ({
       return result.data?.data
     } catch (error) {
       return errorAlert("something went wrong")
+    }
+  },
+
+  // create batch
+  createBatch: async (data) => {
+    try {
+      infoAlert("creating batch ... please wait")
+      let result = await axios.post(instructorEndpoint + "/create-batch", data, { withCredentials: true })
+
+      if (result.data?.code == 401) {
+        errorAlert(result.data?.data)
+        setTimeout(() => {
+          window.location.replace("/login")
+        }, 3000)
+      }
+
+      if (result.data?.status == 0) {
+        if (result.data?.errorCode?.code) infoAlert(result.data.errorCode.code)
+        return errorAlert(result.data?.data)
+      }
+
+      successAlert(result.data?.data)
+      return result.data?.status
+    } catch (error) {
+      return errorAlert("something went wrong")
+    }
+  },
+
+  //get batch by instructor
+  getBatchInstructor: async () => {
+    try {
+      infoAlert("loading data ... please wait")
+
+      let result = await axios.get(instructorEndpoint + "/get-batch", {withCredentials: true})
+
+      if (result.data?.code == 401) {
+        errorAlert(result.data?.data)
+        setTimeout(() => {
+          window.location.replace("/login")
+        }, 3000)
+      }
+
+      if (result.data?.status == 0) {
+        if (result.data?.errorCode?.code) infoAlert(result.data.errorCode.code)
+        errorAlert(result.data?.data)
+        return 0
+      }
+      successAlert("data loaded")
+      return result.data?.data
+
+    } catch (error) {
+      errorAlert("something went wrong")
+      return 0 
+    }
+  },
+
+  // get course by id
+  getCourseById: async (id) => {
+    try {
+      let result = await axios.get(instructorEndpoint + "/course/"+id, { withCredentials: true })
+
+      if (result.data?.code == 401) {
+        errorAlert(result.data?.data)
+        setTimeout(() => {
+          window.location.replace("/login")
+        }, 3000)
+      }
+
+      if (result.data?.status == 0) {
+        if (result.data?.errorCode?.code) infoAlert(result.data.errorCode.code)
+        errorAlert(result.data?.data)
+        return 0
+      }
+      
+      return result.data?.data
+    } catch (error) {
+      errorAlert("something went wrong")
+      return 0
+    }
+  },
+
+  // update course
+  updateCourse: async (data, id) => {
+    try {
+      infoAlert("updating course ... please wait")
+      let result = await axios.post(instructorEndpoint + "/update-course/" + id, data, { withCredentials: true })
+
+      if (result.data?.code == 401) {
+        errorAlert(result.data?.data)
+        setTimeout(() => {
+          window.location.replace("/login")
+        }, 3000)
+      }
+
+      if (result.data?.status == 0) {
+        if (result.data?.errorCode?.code) infoAlert(result.data.errorCode.code)
+        errorAlert(result.data?.data)
+        return 0
+      }
+      successAlert(result.data?.data)
+      return result.data?.status
+    } catch (error) {
+      errorAlert("something went wrong")
+      return 0
     }
   }
 
