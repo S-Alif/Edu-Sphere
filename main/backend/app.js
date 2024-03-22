983174ef2f0dc8f4d0bba0f737e280facf0b1c71
arafatshahariyar <arafatshahariyar1444@gmail.com>
2024-03-22 22:32:29 +0600

    [increase amount of connection]

--- main/backend/app.js
+++ main/backend/app.js
@@ -26,7 +26,7 @@ app.use(express.json({limit: "6mb"}))
 app.use(express.urlencoded({extended: true}))
 
 // rate limiting
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10000});
 app.use(limiter);
 
 // route connection
