8e2b2b15e147024d3257ff41fe58035dbcaaf909
Ancishan <shantodev027@gmail.com>
2024-03-28 19:57:04 +0600

    [UPDATE] Review service query

--- main/backend/src/services/reviewService.js
+++ main/backend/src/services/reviewService.js
@@ -10,8 +10,8 @@ const { getCurrentDateTime } = require('../helpers/helper')
 exports.postInstructorReview = async (req) => {
   try {
     let uid = v4()
-    let query = `INSERT INTO instructor_review (id, studentId, instructorId, review, createdAt) VALUES (?,?,?,?,?);`
-    let data = [uid, req.headers?.id, req.body?.instructorId, req.body?.review, getCurrentDateTime()]
+    let query = `INSERT INTO instructor_review (id, studentId, instructorId, rating, review, createdAt) VALUES (?,?,?,?,?,?);`
+    let data = [uid, req.headers?.id, req.body?.postId, req.body?.rating, req.body?.review, getCurrentDateTime()]
 
     await database.execute(query, data)
 
@@ -36,14 +36,25 @@ exports.getInstructorReview = async (req) => {
   try {
     let instructorId = req.params?.id
 
-    let query = `SELECT ir.*, u.profileImg AS profileImg, u.firstName AS firstName, u.lastName AS lastName
+    let query = `SELECT
+        ir.*,
+        u.profileImg AS profileImg,
+        u.firstName AS firstName,
+        u.lastName AS lastName
     FROM instructor_review AS ir
     JOIN users AS u ON ir.studentId = u.id
     WHERE ir.instructorId = '${instructorId}';`
 
+    let avgQuery = `SELECT
+        AVG(CAST(ir.rating AS DECIMAL(10,2))) AS avgRating
+    FROM instructor_review AS ir
+    WHERE ir.instructorId = '${instructorId}';`
+
+    let avg = await database.execute(avgQuery)
+
     let result = await database.execute(query)
 
-    return { status: 1, code: 200, data: result[0] }
+    return { status: 1, code: 200, data: { data: result[0], avg: avg[0][0].avgRating } }
 
   } catch (error) {
     return { status: 0, code: 200, data: "something went wrong", errorCode: error };
@@ -59,6 +70,12 @@ exports.getInstructorReview = async (req) => {
 exports.postCourseReview = async (req) => {
   try {
     let uid = v4()
+    let query = `INSERT INTO course_review (id, studentId, courseId, rating, review, createdAt) VALUES (?,?,?,?,?,?);`
+    let data = [uid, req.headers?.id, req.body?.postId, req.body?.rating, req.body?.review, getCurrentDateTime()]
+
+    await database.execute(query, data)
+
+    return { status: 1, code: 200, data: "review posted" }
 
   } catch (error) {
     return { status: 0, code: 200, data: "something went wrong", errorCode: error };
@@ -77,6 +94,28 @@ exports.deleteCourseReview = async (req) => {
 // get reviews
 exports.getCourseReview = async (req) => {
   try {
+
+    let courseId = req.params?.id
+
+    let query = `SELECT
+        ir.*,
+        u.profileImg AS profileImg,
+        u.firstName AS firstName,
+        u.lastName AS lastName
+    FROM course_review AS ir
+    JOIN users AS u ON ir.studentId = u.id
+    WHERE ir.courseId = '${courseId}';`
+
+    let avgQuery = `SELECT
+        AVG(CAST(ir.rating AS DECIMAL(10,2))) AS avgRating
+    FROM course_review AS ir
+    WHERE ir.courseId = '${courseId}';`
+
+    let avg = await database.execute(avgQuery)
+
+    let result = await database.execute(query)
+
+    return { status: 1, code: 200, data: { data: result[0], avg: avg[0][0].avgRating } }
 
   } catch (error) {
     return { status: 0, code: 200, data: "something went wrong", errorCode: error };
